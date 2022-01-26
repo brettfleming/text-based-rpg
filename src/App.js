51c4b0c8ea77d681048f54669fd0609cb4d5373a
Brett Fleming <brettfleming015@gmail.com>
2022-01-25 20:37:51 -0600

    working on useState for character States

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from React
+import { useState, useEffect } from React;
+import { Form, Button, Alert } from 'react-bootstrap';
 
 function App() {
   const [characterStates, setCharacterStates] = useState({name: '', race: '', class: '', str: 0, const: 0, dex: 0, wis: 0, int: 0, chr: 0 })
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button>Start</Button>
     </div>
   );
 }
